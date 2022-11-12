cf4fe38349f63af4860d40f5624ec5f3e0479d25
Khangdepzai <khangtgr@gmail.com>
2022-11-12 11:41:53 +0700

    update & add db

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from flask import Flask
-
-# === get necessaries from Flask ==========
 from flask import render_template, jsonify, request
 
+# === use dotenv to config environment ============
+from dotenv import load_dotenv, find_dotenv
+
+# === import sql from Flask =========
+from flask_mysqldb import MySQL
+import os
+
+# === start application with Flask =========
 app = Flask(__name__)
-app.secret_key = 'khangdepzai'
+app.secret_key = '2HKT'
+load_dotenv(find_dotenv())
+
+# === config mysqldb ============
+app.config["MYSQL_HOST"] = os.environ.get("HOST")
+app.config["MYSQL_USER"] = os.environ.get("USER")
+app.config["MYSQL_PASSWORD"] = os.environ.get("PASSWORD")
+app.config["MYSQL_DB"] = os.environ.get("DATABASE")
+mysql = MySQL(app)
 
 
-# === SHOW screen from index ===============
 @app.route("/create_account", methods=['GET', 'POST'])
-# PURPOSE: to show content in the URL: '/', which also means 'localhost:5000' as defaut
 def show_screen_create_account():
-    # PURPOSE: the frontend/UI application would be 'html' file
-    # 'render_template': use the file as the UI/frontend of the app
     return render_template('create_account.html')
 
 
@@ -26,5 +36,6 @@ def show_screen_homepage():
     return render_template('homepage.html')
 
 
+# === Config the port ==============================
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host='localhost')
+    app.run(debug=True, port=5000, host='0.0.0.0')
