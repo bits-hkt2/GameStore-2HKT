5e09e75ea2fae5711a7f2e433e1b61c47595bd02
Khangdepzai <khangtgr@gmail.com>
2022-12-10 16:22:49 +0700

    update edit profile

--- edit_profile.php
+++ edit_profile.php
@@ -47,27 +47,93 @@
                 </h1>
                 <div class="d-flex justent-center align-self-center align-content-center align-items-center w-75 h-75">
                     <!-- FORM SECTIONFORM SECTIONFORM SECTIONFORM SECTIONFORM SECTIONFORM SECTIONFORM SECTIONFORM SECTIONFORM SECTIONFORM SECTIONFORM SECTIONFORM SECTIONFORM SECTION -->
-                    <form method="post" enctype="multipart/form-data" action="./edit_profile.php"
+                    <form method="post" enctype="multipart/form-data" action="edit_profile.php"
                         class="d-flex flex-column flex-fill">
+                        <?php
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "";
+                        $database = "test";
+
+                        $conn = mysqli_connect($servername, $username, $password, $database);
+
+                        if (!$conn) {
+                            echo "Connection failed!";
+                        };
+
+                        session_start();
+                        
+                        if(isset($_POST['submit'])) {
+                            if (isset($_POST['cur_username']) && isset($_POST['new_username']) && isset($_POST['cur_pass']) && isset($_POST['new_pass'])) {
+                                function validate($data){
+                                    $data = trim($data);
+                                    $data = stripslashes($data);
+                                    $data = htmlspecialchars($data);
+                                    return $data;
+                                }
+                                
+                                $cur_username = validate($_POST['cur_username']);
+                                $new_username = validate($_POST['new_username']);
+                                $cur_pass = validate($_POST['cur_pass']);
+                                $new_pass = validate($_POST['new_pass']);
+    
+                                $sql = "SELECT username, password FROM acc_user WHERE username='$cur_username' AND password='$cur_pass'";
+    
+                                $result = mysqli_query($conn, $sql);
+
+                                if (mysqli_num_rows($result) === 1) {
+                                    $row = mysqli_fetch_assoc($result);
+                                    if ($row['username'] === $cur_username && $row['password'] === $cur_pass) {
+                                        $update_sql = "UPDATE acc_user
+                                                       SET username='$new_username', password='$new_pass'
+                                                       WHERE username='$cur_username' AND password='$cur_pass'";
+                                        $con = mysqli_query($conn, $update_sql);
+                                    }else{
+                                        header("Location: index.php?error=Incorect User name or password");
+                                        exit();
+                                    }
+                                }else{
+                                    header("Location: index.php?error=Incorect User name or password");
+                                    exit();
+                                }
+                            
+                            }
+
+                            // $update_sql = "UPDATE acc_user
+                            //                SET username='$new_username', password='$new_pass'
+                            //                WHERE username='$cur_username' AND password='$cur_pass'";
+                                
+                            // $con = mysqli_query($conn, $update_sql);
+
+                                
+                        }
+                        
+                        
+                        ?>
+                        <h4 style="color: red;">
+                            <!-- <?php if(isset($_GET['error'])) echo $_GET['error'] ?> -->
+                        </h4>
                         <div class="form-outline form-white" style="margin: 16px 0">
-                            <input type="text" id="formWhite" class="form-control" maxlength="255"
-                                value="<?php echo $newUser; ?>" />
-                            <label class="form-label" for="formWhite" style="color: rgb(204, 204, 204)">Username</label>
+                            <input type="text" id="formWhite" class="form-control" maxlength="255" name="cur_username"
+                                required value="" />
+                            <label class=" form-label" for="formWhite" style="color: rgb(204, 204, 204)">Current
+                                Username</label>
                         </div>
                         <div class="form-outline form-white" style="margin: 16px 0">
-                            <input type="text" id="formWhite" class="form-control" maxlength="255"
-                                value="<?php echo $newAddress; ?>" />
-                            <label class="form-label" for="formWhite" style="color: rgb(204, 204, 204)">Address</label>
+                            <input type="text" id="formWhite" class="form-control" maxlength="255" name="new_username"
+                                required value="" />
+                            <label class="form-label" for="formWhite" style="color: rgb(204, 204, 204)">New
+                                Username</label>
                         </div>
                         <div class="form-outline form-white" style="margin: 16px 0">
-                            <input type="password" id="formWhite" class="form-control" maxlength="255"
-                                value="<?php echo $oldPassword; ?>" />
+                            <input type="password" id="formWhite" class="form-control" maxlength="255" name="cur_pass"
+                                required value="" />
                             <label class="form-label" for="formWhite" style="color: rgb(204, 204, 204)">Current
                                 Password</label>
                         </div>
                         <div class="form-outline form-white" style="margin: 16px 0">
-                            <input type="password" id="formWhite" class="form-control" maxlength="255"
-                                value="<?php echo $newPassword; ?>" />
+                            <input type="password" id="formWhite" class="form-control" maxlength="255" name="new_pass"
+                                required value="" />
                             <label class="form-label" for="formWhite" style="color: rgb(204, 204, 204)">New
                                 Password</label>
                         </div>
@@ -95,48 +161,50 @@
 
 </body>
 <style>
-    @media (min-width: 768px) {
-        #div-responsive {
-            width: 550px;
-        }
-
-        #back-responsive {
-            margin-left: 5px;
-            margin-bottom: 50px;
-        }
-
-        #update-responsive {
-            margin-right: 5px;
-            margin-bottom: 50px;
-        }
+@media (min-width: 768px) {
+    #div-responsive {
+        width: 550px;
     }
 
-    @media (max-width: 768px) {
-        #button-responsive {
-            display: flex;
-            flex-direction: column;
-            margin: 0;
-        }
-
-        #update-responsive {
-            margin-bottom: 10px;
-            margin-top: -10px;
-            display: flex;
-            width: 100%;
-            justify-content: center;
-        }
-
-        #back-responsive {
-            margin-bottom: 30px;
-            display: flex;
-            width: 100%;
-            justify-content: center;
-        }
-
-        #div-responsive {
-            width: 80%;
-        }
+    #back-responsive {
+        margin-left: 5px;
+        margin-bottom: 50px;
     }
+
+    #update-responsive {
+        margin-right: 5px;
+        margin-bottom: 50px;
+    }
+}
+
+@media (max-width: 768px) {
+    #button-responsive {
+        display: flex;
+        flex-direction: column;
+        margin: 0;
+    }
+
+    #update-responsive {
+        margin-bottom: 10px;
+        margin-top: -10px;
+        display: flex;
+        width: 100%;
+        justify-content: center;
+    }
+
+    #back-responsive {
+        margin-bottom: 30px;
+        display: flex;
+        width: 100%;
+        justify-content: center;
+    }
+
+    #div-responsive {
+        width: 80%;
+    }
+}
 </style>
+
+</html>
 
 </html>
